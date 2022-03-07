a11a8816716fd60c9315d62b16a94272bbc76422
SHINCHVEN <shinchven@gmail.com>
2022-03-07 18:17:28 +0800

    Update nginx.ts

--- src/template-generators/nginx.ts
+++ src/template-generators/nginx.ts
@@ -13,6 +13,7 @@ server {
                 proxy_set_header X-Real-IP $remote_addr;
                 proxy_set_header X-Forwarded-Server $host;
                 proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+                proxy_set_header Host $host;
                 
                 # ========= edit your app's host here =========
                 proxy_pass http://127.0.0.1:80; 
